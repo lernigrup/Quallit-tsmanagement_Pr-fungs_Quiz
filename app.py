161102c4933349e725cd4dc3ef5c771a599ab0ab
lernigrup <lernigrup@gmail.com>
2026-06-23 14:40:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -42,6 +42,7 @@ QUIZZES = {
     "Probeklausur 1 2026": "Probeklausur 1 2026.json",
     "Probeklausur 2 2026": "Probeklausur 2 2026.json",
     "Probeklausur 3 2026": "Probeklausur 3 2026.json",
+    "Alle Fragen 2026": "Alle Fragen 2026.json",
 }
 
 # Fallback (if you keep using a single questions.json)
